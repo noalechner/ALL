d0d29e6226bbe85a018b033eb986f91e38438d41
Ben the winner <ben6119070@gmail.com>
2024-11-29 10:42:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.afinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button button;
+    private EditText email;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseApp.initializeApp(this);
+        button = findViewById(R.id.button);
+        email = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        ref.child("STUDENTS").child("studentsId").setValue("volunteerPlace");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sEmail = email.getText().toString().trim();
+                String sPassword = password.getText().toString().trim();
+                if(TextUtils.isEmpty(sEmail)|| TextUtils.isEmpty(sPassword))
+                {
+                    Toast.makeText(MainActivity.this, "error, please try again ", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.signInWithEmailAndPassword(sEmail,sPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(MainActivity.this, "good job! ", Toast.LENGTH_SHORT).show();
+                        }
+                    }); //לא יכול להיות במיין
+                }
+
+
+
+            }
+        });
+    }
+}
