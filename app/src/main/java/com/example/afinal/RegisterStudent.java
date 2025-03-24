05a95f31884152c577670a9d984c4b330006f101
Ben the winner <ben6119070@gmail.com>
2025-03-24 23:18:50 +0200

    I learned how to reaf from DataBase- everything working hamsa

--- app/src/main/java/com/example/afinal/RegisterStudent.java
+++ app/src/main/java/com/example/afinal/RegisterStudent.java
@@ -2,6 +2,7 @@ package com.example.afinal;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,13 +10,22 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
+
 public class RegisterStudent extends AppCompatActivity {
     String[] items = {"Volunteen Type","חיות", "חקלאות", "ניצולי שואה","חולי סרטן"};
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +40,28 @@ public class RegisterStudent extends AppCompatActivity {
 
         Spinner spinner = findViewById(R.id.spinnerVolunTypes);
 
-        // Create an ArrayAdapter using a simple spinner item layout and the string array
+            // Create an ArrayAdapter using a simple spinner item layout and the string array
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_reg_student_each, items);
         adapter.setDropDownViewResource(R.layout.view_dropdown_item);
+        //****** ככה אנחנו קוראים מהפיירבייס
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("HostEvents");
+        reference.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
+                                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                                    Log.d("dataSnapshot", snapshot.getValue().toString());
+                                                }
+                                            }
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        // Set the adapter to the spinner
+                                            }
+
+                                        });
+                // ******
+
+
+                // Set the adapter to the spinner
         spinner.setAdapter(adapter);
 
         // Handle Spinner item selection
