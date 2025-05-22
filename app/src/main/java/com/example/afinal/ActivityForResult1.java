8d399650cc3e133e6a586cf09909d47d82a069b0
Ben the winner <ben6119070@gmail.com>
2025-05-23 01:13:10 +0300

    onActivityResult is on the way. details on TopicAnimals

---
+++ app/src/main/java/com/example/afinal/ActivityForResult1.java
@@ -0,0 +1,55 @@
+package com.example.afinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityForResult1 extends AppCompatActivity {
+    private Button agree;
+    private Button decline;
+    private TextView dataView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_for_result1);
+        agree= findViewById(R.id.agree);
+        decline= findViewById(R.id.decline);
+        dataView= findViewById(R.id.text);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        String dataString = intent.getStringExtra("data");
+        dataView.setText(dataString);
+        agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(1, intent);
+                finish();
+            }
+        });
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(0, intent);
+                finish();
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/afinal/TopicAnimals.java
+++ app/src/main/java/com/example/afinal/TopicAnimals.java
@@ -1,9 +1,17 @@
 package com.example.afinal;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -18,17 +26,24 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TopicAnimals extends AppCompatActivity {
     private String topicA="animals";
     ArrayList<String> itemsOfTopic = new ArrayList<>();
     String TAG="TopicAnimals";
+    private Button launcher1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_topic_animals);
+        launcher1 = findViewById(R.id.launcher);
+
+// Create a HashMap
+        HashMap<String, String> volunteerHashMap = new HashMap<>();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -41,20 +56,47 @@ public class TopicAnimals extends AppCompatActivity {
             @Override
             public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    String key = snapshot.getKey();
-//                    itemsOfTopic.add(key);
-                    String key = snapshot.getValue().toString();
-                    Log.d(TAG,key);
-
+                    //this is the key that should appear in the spinner
+                    String key1 = snapshot.getKey();
+                    itemsOfTopic.add(key1);
+                    //this is the value of the key
+                    String value = snapshot.getValue().toString();
+                    volunteerHashMap.put(key1,value);
+                    Log.d(TAG,value);
                 }
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.d(TAG,"data base canclled" + databaseError);
-
             }
-
         });
         // ******
+        ActivityResultLauncher<Intent> detailActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG,"returned data " + result.getResultCode());
+                        if (result.getResultCode() == 1) {
+                            Log.d(TAG,"user agreed!");
+                        }
+                        if (result.getResultCode() == 0) {
+                            Log.d(TAG,"user declined");
+                        }
+                            // There are no request codes
+//                            Intent data = result.getData();
+
+//                        }
+                    }
+                });
+        launcher1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ActivityForResult1.class);
+                String dataToBeTransfered = "abc";
+                i.putExtra("data",dataToBeTransfered);
+                detailActivityResultLauncher.launch(i);
+            }
+        });
     }
 }
